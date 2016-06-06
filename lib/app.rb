10bfd5a9effd0683e47df1751dc30dd1506bab9d
Stephen Dawes <sdawes@outlook.com>
2016-06-06 21:44:58 +0100

    route cat, html and css applied

---
+++ lib/app.rb
@@ -0,0 +1,15 @@
+require 'sinatra'
+
+get '/' do
+  "Hello World"
+end
+
+get '/secret' do
+  "WTF is this??"
+end
+
+get '/cat' do
+  "<div style='border: 3px dashed red'>
+    <img src='http://bit.ly/1eze8aE'>
+   </div>"
+end
